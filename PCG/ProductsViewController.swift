9aef005907266aa00c54e1e91ed18e03fdf0c552
Robert Keffury <bobby.keffury@me.com>
2020-04-22 14:52:25 -0700

    Changed CollectionVC to VC.

---
+++ PCG/ProductsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProductsViewController.swift
+//  PCG
+//
+//  Created by Bobby Keffury on 4/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Methods
+    
+    //MARK: - Actions
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
