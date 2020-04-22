7a0a77deff7956e7e0c4b9eacd81fb9792ad4a57
Robert Keffury <bobby.keffury@me.com>
2020-04-22 15:59:12 -0700

    Added History Table View With Reusable Cell.

---
+++ PCG/HistoryTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryTableViewCell.swift
+//  PCG
+//
+//  Created by Bobby Keffury on 4/22/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+}
