e295fbe7ce4a7ed8f4f54222a5a1a7b8fa1e4147
Robert Keffury <bobby.keffury@me.com>
2021-01-26 19:39:18 -0800

    Linked up sign up & login

---
+++ PCG/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  PCG
+//
+//  Created by Bobby Keffury on 1/26/21.
+//  Copyright © 2021 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    // MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
---
+++ PCG/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  PCG
+//
+//  Created by Bobby Keffury on 1/26/21.
+//  Copyright © 2021 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    // MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
